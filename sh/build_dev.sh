4486c1b08ca55df8c815aa79405af42910fe38e8
iron <defiiron@gmail.com>
2021-01-05 15:51:59 +0800

    Docker lend-api

--- sh/build_dev.sh
+++ sh/build_dev.sh
@@ -6,9 +6,9 @@ npm test
 
 yarn build:prod
 
-docker build -t  coinflow/lend-data:dev .
+docker build -t  coinflow/lend-api:dev .
 
 docker login --username foxdex -p $DOCKER_ACCESS_TOKEN
 
 
-docker push coinflow/lend-data:dev
+docker push coinflow/lend-api:dev
