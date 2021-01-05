2ee52a428d263a302e905a769d915c6de131d846
iron <defiiron@gmail.com>
2021-01-05 15:41:25 +0800

    Rm web and lend

--- sh/restart_dev.sh
+++ sh/restart_dev.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-docker login --username lend -p $DOCKER_ACCESS_TOKEN
+docker login --username foxdex -p $DOCKER_ACCESS_TOKEN
 
-docker rmi -f lend/node:latest
-docker-compose  -f  docker-compose-dev.yml  pull lend
+docker rmi -f coinflow/lend-api:dev
+docker rmi -f coinflow/lend-web:dev
+docker-compose  -f  docker-compose-dev.yml  pull lend web
 
 docker-compose -f docker-compose-dev.yml down
 docker-compose -f docker-compose-dev.yml up -d
