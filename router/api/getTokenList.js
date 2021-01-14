2b748f6dd873bc3b5c4a1d3ac72b496b4533a8a1
zhangyang <1105626801@qq.com>
2021-01-14 14:09:49 +0800

    apy

--- router/api/getTokenList.js
+++ router/api/getTokenList.js
@@ -74,7 +74,7 @@ try {
         totalMint += token[i].mint_scale * token[i].current_price;
         totalBorrow += token[i].borrow_scale * token[i].current_price;
     }
-    Apy = ((rate * 365) / total).toFixed(2) *1
+    Apy = (rate  / total).toFixed(2) *1
 
 
     healthIndex = await HealthIndex(req, res, name);
